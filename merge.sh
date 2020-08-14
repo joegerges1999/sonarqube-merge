12e5dc21c82114a4119b5b686926f10b864fd398
GERGES Joe <jgerges@murex.com>
2020-08-14 12:58:06 +0300

    added version number and version for non -community images

--- merge.sh
+++ merge.sh
@@ -3,10 +3,11 @@ APP=$1
 TEAM=$2
 VERSION=$3
 HOSTNAME=$4
+VERSION_NUMBER=$(echo "$VERSION" | rev | cut -d "-" -f2 | rev)
 CLUSTER_ID=c-jpxcn
 PROJECT_ID=p-zwxgj
 
-if [[ -z $APP || -z $TEAM || -z $VERSION || -z $HOSTNAME ]]; then
+if [[ -z $APP || -z $TEAM || -z $VERSION || -z $HOSTNAME || -z $VERSION_NUMBER ]]; then
   echo 'One or more variables are undefined, exiting script ...'
   exit 1
 fi
@@ -15,7 +16,7 @@ echo "Logging in to rancher ..."
 rancher login https://rancher.cd.murex.com/ --token token-vkq9d:wg8gtt4gbgtk7nzfhlj4gs87dn4w2hxhd9qmcb9fmnqkllgx57792r --context $CLUSTER_ID:$PROJECT_ID
 
 echo "Creating new SonarQube instance"
-rancher app install --values /data/$TEAM/$APP/migration/myvals.yaml --set hostname="$HOSTNAME" --set team="$TEAM" --set sonarqube.image.tag="$VERSION-community"  --version 0.1.0 --namespace $APP $APP $TEAM-$APP
+rancher app install --values /data/$TEAM/$APP/migration/myvals.yaml --set hostname="$HOSTNAME" --set team="$TEAM" --set sonarqube.image.tag="$VERSION"  --version 0.1.0 --namespace $APP $APP $TEAM-$APP
 
 ansible-playbook /data/$TEAM/$APP/migration/check-readiness.yaml --extra-vars "web_context=/sonar hostname=$HOSTNAME"
 
@@ -44,12 +45,12 @@ if [[ -z $CONF_PATH || -z $DATA_PATH || -z $EXTENSIONS_PATH || -z $LOGS_PATH || 
 fi
 
 echo "Copying SonarQube files to nfs ..."
-unzip /data/$TEAM/$APP/documents/$APP-$VERSION.zip -d /data/$TEAM/$APP/documents/
+unzip /data/$TEAM/$APP/documents/$APP-$VERSION_NUMBER.zip -d /data/$TEAM/$APP/documents/
 rm -rf /mnt/nfs/$DATA_PATH/* /mnt/nfs/$CONF_PATH/* /mnt/nfs/$EXTENSIONS_PATH/* /mnt/nfs/$LOGS_PATH/*
-cp -r /data/$TEAM/$APP/documents/$APP-$VERSION/data/* /mnt/nfs/$DATA_PATH/
-cp -r /data/$TEAM/$APP/documents/$APP-$VERSION/conf/* /mnt/nfs/$CONF_PATH/
-cp -r /data/$TEAM/$APP/documents/$APP-$VERSION/extensions/* /mnt/nfs/$EXTENSIONS_PATH/
-cp -r /data/$TEAM/$APP/documents/$APP-$VERSION/logs/* /mnt/nfs/$LOGS_PATH/
+cp -r /data/$TEAM/$APP/documents/$APP-$VERSION_NUMBER/data/* /mnt/nfs/$DATA_PATH/
+cp -r /data/$TEAM/$APP/documents/$APP-$VERSION_NUMBER/conf/* /mnt/nfs/$CONF_PATH/
+cp -r /data/$TEAM/$APP/documents/$APP-$VERSION_NUMBER/extensions/* /mnt/nfs/$EXTENSIONS_PATH/
+cp -r /data/$TEAM/$APP/documents/$APP-$VERSION_NUMBER/logs/* /mnt/nfs/$LOGS_PATH/
 
 echo "Copying migration scripts and database dump to nfs ..."
 if [ ! -d "/mnt/nfs/$PG_PATH/migration-scripts" ]; then
@@ -73,9 +74,9 @@ echo "Cleaning up volume from migration scripts ..."
 rm -rf /mnt/nfs/$PG_PATH/migration-scripts
 
 echo "Restarting the service ..."
-rancher app upgrade --values /data/$TEAM/$APP/migration/myvals.yaml --set replicaCount='0' --set hostname="$HOSTNAME" --set team="$TEAM" --set sonarqube.image.tag="$VERSION-community"  $TEAM-$APP 0.1.0
+rancher app upgrade --values /data/$TEAM/$APP/migration/myvals.yaml --set replicaCount='0' --set hostname="$HOSTNAME" --set team="$TEAM" --set sonarqube.image.tag="$VERSION"  $TEAM-$APP 0.1.0
 sleep 7s
-rancher app upgrade --values /data/$TEAM/$APP/migration/myvals.yaml --set hostname="$HOSTNAME" --set team="$TEAM" --set sonarqube.image.tag="$VERSION-community"  $TEAM-$APP 0.1.0
+rancher app upgrade --values /data/$TEAM/$APP/migration/myvals.yaml --set hostname="$HOSTNAME" --set team="$TEAM" --set sonarqube.image.tag="$VERSION"  $TEAM-$APP 0.1.0
 
 echo "Rechecking readiness ..."
 ansible-playbook /data/$TEAM/$APP/migration/check-readiness.yaml --extra-vars "web_context=/sonar hostname=$HOSTNAME"
