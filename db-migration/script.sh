ab152b38019f66be82f72eef914182adb34b724a
joegerges1999 <67051137+joegerges1999@users.noreply.github.com>
2020-08-17 18:34:15 +0300

    Update script.sh

--- db-migration/script.sh
+++ db-migration/script.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 ROOTDIR=/var/lib/postgresql
 
-echo "Removing connections and dropping database ..."
+echo "[$(date)] INFO: Removing connections and dropping database ..."
 psql -U sonar postgres -f $ROOTDIR/migration-scripts/drop-table.sql
 
-echo "Restoring database ..."
+echo "[$(date)] INFO: Restoring database ..."
 createdb -U sonar sonar
 psql -U sonar sonar < $ROOTDIR/backups/db_dump.sql 
 
-echo "Allowing connections back up ..."
+echo "[$(date)] INFO: Allowing connections back up ..."
 psql -U sonar postgres -f $ROOTDIR/migration-scripts/allow-connections.sql
